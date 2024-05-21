baaa2b49a9a525899dcf676fd5184f73f18fcb68
webala <webd01@protonmail.com>
2024-05-21 08:32:09 +0300

    some change

--- src/pages/dashboard/components/DataTablePagination.tsx
+++ src/pages/dashboard/components/DataTablePagination.tsx
@@ -2,7 +2,6 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6'
 import { MdOutlineKeyboardDoubleArrowLeft, MdOutlineKeyboardDoubleArrowRight } from 'react-icons/md'
 import { type Table } from '@tanstack/react-table'
 import { Button } from 'src/components/ui/button'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from 'src/components/ui/select'
 
 interface DataTablePaginationProps<TData> {
   table: Table<TData>
@@ -11,11 +10,8 @@ interface DataTablePaginationProps<TData> {
 export function DataTablePagination<TData>({ table }: DataTablePaginationProps<TData>) {
   return (
     <div className="flex items-center justify-between px-2">
-      <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex items-center space-x-2">
+        {/* <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
             value={`${table.getState().pagination.pageSize}`}
@@ -34,7 +30,7 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
               ))}
             </SelectContent>
           </Select>
-        </div>
+        </div> */}
         <div className="flex w-[100px] items-center justify-center text-sm font-medium">
           Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
         </div>
